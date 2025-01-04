79935f4572803539a239bf239ec47195a07057c7
Tuğberk BİLEN <tgbrkblnn@gmail.com>
2025-01-04 22:16:56 +0300

    v2

--- src/Runner.java
+++ src/Runner.java
@@ -2,6 +2,8 @@ public class Runner {
     public static void main(String[] args) {
         //versiyon 1
         System.out.println("merhabalar");
+        //versiyon 2
+        System.out.println("nasılsın");
 
     }
 }
